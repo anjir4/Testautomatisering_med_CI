741c5e4e11be54c03dff5354ffbe66f2a150d048
anjir4 <anjir4@gmail.com>
2023-03-05 19:36:04 +0100

    Test for checking upload time added

--- test_tetrifact.py
+++ test_tetrifact.py
@@ -5,6 +5,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from subprocess import run
 import os
+import urllib.parse
+import datetime
 
 #This will open this webpage
 #driver.get("https://tetrifact.manafeed.com/")
@@ -21,20 +23,23 @@ import os
 #this will submit element seach box?
 #search_box.submit()
 
-class basePageObjectTest(TestCase):
+class myTestClass(TestCase):
     
     def __init__(self, methodName: str = "runTest") -> None:
         super().__init__(methodName)
+        #class variables
         self.driver = None
         self.tetrifactUrl = 'https://testrifact.manafeed.com'
         self.packageId = 'test_package'
 
     def setUp(self):
         """
-        setting up the test
+        setting up the test:
+
+        Upload said package to Tetrifact page.
+        Set PATH and assign the webdriver.
         """
         dir_path = os.path.dirname(os.path.realpath(__file__))
-
         zipPath = os.path.join(dir_path,f'{self.packageId}.zip')
         packageRootDir = os.path.join(dir_path,'package_content')
 
@@ -50,6 +55,7 @@ class basePageObjectTest(TestCase):
             print(f'Error removing zip: {zipPath} : {e}')
             raise e
 
+        #Zip 3 files into a 7z package.
         result = run(
             ['7z', 
             'a' ,
@@ -59,6 +65,7 @@ class basePageObjectTest(TestCase):
 
         print(f'zip results:{result}')
 
+        #Upload the package to the Tetrifact page
         result = run(
             ['curl',
             '--write-out', '%{http_code}%', 
@@ -72,26 +79,78 @@ class basePageObjectTest(TestCase):
 
         print(f'upload results:{result}')
 
+        #Set PATH
         PATH = "D:\Tools\chromedriver110.exe"
-        self.driver = webdriver.Chrome(PATH) # Optional argument, if not specified will search path.
 
-        self.driver.get(self.tetrifactUrl)
-
-        #Documentation for select By
-        #https://selenium-python.readthedocs.io/locating-elements.html 
-        WebDriverWait(self.driver, 10).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "title")))
+        #Assign webdriver with PATH as argument
+        self.driver = webdriver.Chrome(PATH)
     
     def test_title_text(self):
+        """
+        Testmetod för att få till ett första lyckat test,
+        att sidans Titeltext matchar det förväntade värdet.
+        """   
 
-        #making sure this object has loaded, as then we can assume that this
-        #relatively simple page has fully loaded and is ready for testing.
+        #Load the Tetrifact page
+        self.driver.get(self.tetrifactUrl)
+
+        #En explicit wait för ett element för att säkerställa att
+        #sidan börjat laddas. Inte en garanti för fulladdad sida, men
+        #en basic check.
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "title")))
 
         title = self.driver.find_element(By.CLASS_NAME, "title")
         self.assertEqual(title.text,"Tetrifact Artifact storage")
 
+    def test_upload_time(self):
+        """
+        Metod som kontrollerar att uppladdningstiden
+        stämmer med nuvarande tid (inom 1 min intervall). 
+        """
+
+        url = urllib.parse.urljoin(self.tetrifactUrl, f"package/{self.packageId}", )
+        #Load the package page
+        self.driver.get(url)
+
+        #En explicit wait för ett element för att säkerställa att
+        #sidan börjat laddas. Inte en garanti för fulladdad sida, men
+        #en basic check.
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "title")))
+        
+        #finner elementet med texten Created.
+        element = self.driver.find_element(By.XPATH, "//div[contains(.,'Created')]/following-sibling::*") # 
+        #använder elementet för att få ut texten som
+        #anger när uppladdningen skett, i str format
+        date = element.text
+        date_cleaned = date[0:16] #rensar strängen
+        date_as_datetime = datetime.datetime.strptime(date_cleaned, '%Y-%m-%d %H:%M')
+        self.assertAlmostEqual(datetime.datetime.now(), date_as_datetime, delta=datetime.timedelta(seconds=5000))
+
+        print("hey")
+    """
+    def test_file_count(self):
+        pass
+
+    def test_file_size(self):
+        pass
+
+    def test_tags(self):
+        pass
+
+    def test_file_names(self):
+        pass
+    """
+
     def tearDown(self) -> None:
+        """
+        Tearing down the test:
+        Closing the driver and removing 
+        uploaded package.
+        """
         self.driver.quit()
+
 
         run(
             ['curl',
